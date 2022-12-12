63808c17619b58a4fff4d1ff4d5cbcec918cdd3e
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-12-12 12:35:56 -0500

    added image URL key in schema

--- models/seed.js
+++ models/seed.js
@@ -1,7 +1,8 @@
 module.exports = {
+    imageURL: "https://image.uniqlo.com/UQ/ST3/us/imagesgoods/459207/sub/usgoods_459207_sub7.jpg?width=750",
     type: "shirt",
-    brand: "addidas",
-    size: "large",
-    color: "black",
+    brand: "UNIQLO",
+    size: "medium",
+    color: "beige",
     isClean: true
 }
