bc094f44176fbef7567f0b4d051fdf82527fd8a8
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-04 15:57:52 -0500

    updated database link

--- server.js
+++ server.js
@@ -81,6 +81,6 @@ app.listen(process.env.PORT || 3000, ()=>{
 	console.log('listening'); 
 })
 
-mongoose.connect('mongodb+srv://samhan104:hHkGCYhVATr1oHGi@cluster0.gr8z5kk.mongodb.net/?retryWrites=true&w=majority', () => {
+mongoose.connect('mongodb+srv://samhan104:yxbxbUBkYmB2Msyt@cluster0.gr8z5kk.mongodb.net/?retryWrites=true&w=majority', () => {
         console.log("connected mongodb")
     })
